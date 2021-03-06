787fb33133a66adffce76956b2c090de329d0eb2
thanapat prommin <thanapatpro@gosoft.co.th>
2021-03-06 19:27:34 +0700

    init

---
+++ src/main/java/com/chillout/oauth2/oauth2demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.chillout.oauth2.oauth2demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Oauth2demoApplication.class);
+	}
+
+}
